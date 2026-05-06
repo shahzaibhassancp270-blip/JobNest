de82adf871e34b45ac90a0f165750a888c49f817
shahzaib hassan <su92-bssem-s23-002@superior.edu.pk>
2026-05-07 02:04:33 +0500

    security: Remove API keys from repo and add to .gitignore

--- lib/core/constants/api_keys.dart
+++
@@ -1,5 +0,0 @@
-// lib/core/constants/api_keys.dart
-class ApiKeys {
-  static const String rapidApiKey = '854dc2968fmsh8201f12d9ea1979p1e3107jsn6de12e4930cb';
-  static const String rapidApiHost = 'jsearch.p.rapidapi.com';
-}
